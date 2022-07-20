70817ee8508dc09bd6da71e9d67db6c2334f9103
Shawns0826 <53288817+Shawns0826@users.noreply.github.com>
2022-07-20 00:57:46 -0400

    Update RegisterScreen.js

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -25,8 +25,7 @@ export default class LoginScreen extends Component {
     return (
       <View style= {styles.mainContainer}>
         <View style= {styles.container}>
-          <View style= {styles.loginBox}>
-            <ScrollView>
+            <ScrollView contentContainerStyle={styles.loginBox}>
                 <Text style={styles.login}>Sign Up</Text>
                 <Text style= {[styles.text, {marginRight:250}]}>Display Name</Text>
                 <TextInput style={[styles.textInput, {backgroundColor: this.state.isRegisterNameFocus ? '#FEFAE0' : '#9aa871'}]}
@@ -71,7 +70,6 @@ export default class LoginScreen extends Component {
             <Text style= {{color: '#7C5227', fontSize: 15, fontWeight: '500', textDecoration: 'underline'}}>Already have a account? Click here to log in</Text>
             </TouchableOpacity>
             </ScrollView>
-            </View>
             </View>
         </View>
     )
