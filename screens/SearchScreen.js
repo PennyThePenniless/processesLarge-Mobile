026d43247a3475d69b38bc0e3fac8691027fa22a
Shawns0826 <53288817+Shawns0826@users.noreply.github.com>
2022-07-20 20:12:40 -0400

    Update SearchScreen.js

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -31,7 +31,7 @@ const styles = StyleSheet.create({
     },
     columnBox: {
         flexDirection: 'row',
-        flex: .3,
+        flex: 2,
     },
     box1: {
         backgroundColor: 'blue',
