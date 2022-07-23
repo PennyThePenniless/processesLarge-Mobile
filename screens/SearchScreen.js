15a13cbc68eef7fdc5ae54f33fd8aed28f490e49
Shawns0826 <53288817+Shawns0826@users.noreply.github.com>
2022-07-22 22:33:22 -0400

    Update SearchScreen.js

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, {Component, useState} from 'react';
-import { StyleSheet, Button, StatusBar, Text, TextInput, View, ScrollView } from 'react-native';
+import { StyleSheet, Button, StatusBar, Text, TextInput, View, ScrollView, TouchableOpacity } from 'react-native';
 
 
 export default class SearchScreen extends Component {
