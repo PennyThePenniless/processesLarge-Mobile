2cffec7fee3ee0c0050e87f66109037bd27edda6
Shawns0826 <53288817+Shawns0826@users.noreply.github.com>
2022-07-22 20:55:24 -0400

    Update SearchScreen.js

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -85,6 +85,7 @@ const styles = StyleSheet.create({
     foodBox: {
         backgroundColor: 'purple',
         height: 120,
+        flexDirection, 'row'
         },
     picture: {
         flex: 1,
