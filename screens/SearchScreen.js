278b38e5a1b94933c496d7074ac452717409d687
Shawns0826 <53288817+Shawns0826@users.noreply.github.com>
2022-07-22 21:09:56 -0400

    Update SearchScreen.js

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -116,7 +116,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'purple',
         height: 120,
         flexDirection: 'row',
-        padding: 15,
+        padding: 5,
         },
     picture: {
         flex: 1,
