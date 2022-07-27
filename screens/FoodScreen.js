915dd9cda6bb513f3d6f22840cfe0b09f1378825
Shawns0826 <53288817+Shawns0826@users.noreply.github.com>
2022-07-27 18:42:23 -0400

    Update FoodScreen.js

--- screens/FoodScreen.js
+++ screens/FoodScreen.js
@@ -66,18 +66,7 @@ constructor()
                         
                         <View style={styles.ingredients}>
                             <FlatList
-        data={[
-          {key: 'Devin'},
-          {key: 'Dan'},
-          {key: 'Dominic'},
-          {key: 'Jackson'},
-          {key: 'James'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jillian'},
-          {key: 'Jimmy'},
-          {key: 'Julie'},
-        ]}
+        data={object.ingredients}
         renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
       />
                         </View>
