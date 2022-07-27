ca7d6816387b6f03b8ac7e3a233027c2fa9829d6
Shawns0826 <53288817+Shawns0826@users.noreply.github.com>
2022-07-27 16:52:38 -0400

    Update FoodScreen.js

--- screens/FoodScreen.js
+++ screens/FoodScreen.js
@@ -4,7 +4,7 @@ import { StyleSheet, Button, StatusBar, Text, TextInput, View, ScrollView, Touch
 
 
 export default class FoodScreen extends Component {
-    getRecipe = (id) => {
+   const getRecipe = (id) => {
     // TODO: API call
     return({
     title: 'Pizza',
@@ -19,7 +19,6 @@ export default class FoodScreen extends Component {
   })
   
 };
-   recipe = getRecipe();
     constructor() 
   {
     super()
