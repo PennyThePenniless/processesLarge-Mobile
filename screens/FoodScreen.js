6065d7fc36e59600e5e16dc812abe8818b8c8972
Shawns0826 <53288817+Shawns0826@users.noreply.github.com>
2022-07-27 19:10:43 -0400

    Update FoodScreen.js

--- screens/FoodScreen.js
+++ screens/FoodScreen.js
@@ -71,12 +71,14 @@ constructor()
                         </View>
                         
                         <View style={styles.ingredients}>
+                            <Text>Ingredients:</Text>
                             <FlatList
         data={list}
         renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
       />
                         </View>
                         <View style={styles.instructions}>
+                            <Text>Instructions:</Text>
                            <Text>{object.instructions}</Text>
                         </View>
                         
