c20f25fb1ea4ae8fcf05114a89b082533458df34
Shawns0826 <53288817+Shawns0826@users.noreply.github.com>
2022-07-26 20:15:04 -0400

    Update FoodScreen.js

--- screens/FoodScreen.js
+++ screens/FoodScreen.js
@@ -23,7 +23,7 @@ export default class FoodScreen extends Component {
                 <View style={styles.box3}>
                     
                         <View style={styles.nutrition}>
-                             <Text style={{justifyContent: 'center'}}>Nutrition</Text>
+                             <Text style={{textAlign: 'center'}}>Nutrition</Text>
                         </View>
                         <View style={styles.recipe}>
                              <Text>Recipe</Text>
