3f6a84be42b7473e14f3e67defd56686c2992272
Shawns0826 <53288817+Shawns0826@users.noreply.github.com>
2022-07-27 19:01:13 -0400

    Update FoodScreen.js

--- screens/FoodScreen.js
+++ screens/FoodScreen.js
@@ -171,7 +171,7 @@ const styles = StyleSheet.create({
         //flex: 1,
         textAlign: 'center',
         //backgroundColor: 'blue'
-        borderWidth: 5,
+        borderWidth: 1,
     },
     ingredients: {
         flex: 1,
