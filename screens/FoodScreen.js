8a990b4b3054753fa73fa6704de2b826b7bcf824
Shawns0826 <53288817+Shawns0826@users.noreply.github.com>
2022-07-27 20:15:19 -0400

    Update FoodScreen.js

--- screens/FoodScreen.js
+++ screens/FoodScreen.js
@@ -66,7 +66,7 @@ constructor()
                                <Text style={{color: '#7c5227'}}>[Image]</Text>
                     </View>
                         <View style={styles.recipe}>
-                                   <Text style={{justifyContent: 'center',color:'#7c5227'>Recipe</Text>
+                                   <Text style={{justifyContent: 'center',color:'#7c5227'}}>Recipe</Text>
                         </View>
                         </View>
                        <View style={styles.ingredients}>
