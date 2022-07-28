f3fb2032bbb27593e3c52e89d9412953787b966d
Shawns0826 <53288817+Shawns0826@users.noreply.github.com>
2022-07-27 20:39:16 -0400

    Update FoodScreen.js

--- screens/FoodScreen.js
+++ screens/FoodScreen.js
@@ -32,6 +32,35 @@ list = list.map(x => {
   return ({ key: x });
 })
 
+ let recipeName = "Test";
+    const [made, setMade] = useState();
+    const [saved, setSaved] = useState();
+
+    useEffect( () => {
+        if(made) {
+            madeSrc = PotClicked
+            //add recipe to db
+        }
+        else {
+            madeSrc = Pot
+            //remove recipe from db
+        }
+    }, [made]);
+
+    useEffect( () => {
+        if(saved) {
+            let savedSrc = HeartClicked
+            //add recipe to db
+        }
+        else {
+            let savedSrc = Heart
+            //remove from db
+        }
+    }, [saved]);
+
+    let madeSrc = PotClicked;
+    let savedSrc = HeartClicked;
+
 export default class FoodScreen extends Component {
    
  
